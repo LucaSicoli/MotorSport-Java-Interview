9655675bae8e4ddcde17ac205caeb45c5839d7ce
Luca sicoli <luca.sicoli12@hotmail.com>
2024-11-06 21:59:59 -0300

    functional

--- src/main/java/com/motorsport/MotorsportApplication.java
+++ src/main/java/com/motorsport/MotorsportApplication.java
@@ -1,13 +1,24 @@
 package com.motorsport;
 
+import com.motorsport.service.MotoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MotorsportApplication {
+public class MotorsportApplication implements CommandLineRunner {
+
+	@Autowired
+	private MotoService motoService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MotorsportApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Fetching motorcycle data...");
+		motoService.obtenerPromedioPorMarca(); // Automatically perform a GET request when the application starts
+	}
 }
